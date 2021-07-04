88149f9a7407bb0f255e68b9295c00e29ef28408
kenji81 <ed.cespedesa@gmail.com>
2021-07-04 11:48:15 -0400

    Primer commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script de Intalacion y Configuracion de De Debian 10
+# Actualizacion del S.O.
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade;
+# Istalado paquetes necesarios
+sudo apt install wget curl git neofetch net-tools dnsutils nmap;
+Clonando repositoprio
+git clone https://github.com/GorillaTi/setup-debian.git;
